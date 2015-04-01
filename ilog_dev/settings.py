f5c54d70d2ce2e76b348092828b3ecf12d1e1e0f
sprksh <sprksh.j@gmail.com>
2015-04-01 10:16:15 +0530

    another test

--- ilog_dev/settings.py
+++ ilog_dev/settings.py
@@ -3,7 +3,7 @@ Django settings for ilog_dev project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
+# lets understand git
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
